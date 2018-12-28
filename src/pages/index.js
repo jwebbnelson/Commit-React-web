63240007b702db13ec0e654d09c8a5741a40e6d0
Jordan Nelson <jordanwebbnelson@gmail.com>
2018-12-28 14:32:58 -0700

    Add monthView screenshot

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import screenshot from '../images/Month_View.png'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-  <div>
-    <div className="Hero">
-      <div className="HeroGroup">
-        <h1>Commit to new habits today!</h1>
-        <p>The Commit App helps you commit to new habits, not just track them.</p>
-        <p>We’re not the first habit app.<br></br> We’re the best!</p>
-        <Link to="/page-2/">Download</Link>
+    <div>
+      <div className="Hero">
+        <div className="HeroContainer">
+          <div className="HeroImage">
+            <img src={screenshot} />
+          </div>
+          <div className="HeroGroup">
+            <h1>Commit to new habits today!</h1>
+            <p>The Commit App helps you commit to new habits, not just track them.</p>
+            <p>We’re not the first habit app.<br></br> We’re the best!</p>
+            <Link to="/page-2/">Download</Link>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   </Layout>
 )
 
